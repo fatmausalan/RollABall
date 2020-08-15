85b7d9da1305ffa4a0cac209a5f169fd65b0606f
Fatma Usalan <b21727829@cs.hacettepe.edu.tr>
2020-08-15 17:43:36 +0300

    RollABall game add

---
+++ sourcecode/RollingBall/Assets/codes/ballcontrol.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ballcontrol : MonoBehaviour
+{
+    Rigidbody phy;
+    public int speed;
+    public int toplanacakobjesayisi;
+    int sayac = 0;
+
+    public Text oyunbittiText;
+    public Text sayacText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        phy = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float hor = Input.GetAxisRaw("Horizontal");
+        float ver = Input.GetAxisRaw("Vertical");
+
+        Vector3 vec = new Vector3(hor, 0, ver);
+
+        phy.AddForce(vec*speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "engel")
+        {
+            //Destroy(other.gameObject);
+            other.gameObject.SetActive(false);
+            sayac++;
+            sayacText.text = "" + sayac;
+        }
+
+        if (sayac == toplanacakobjesayisi)
+        {
+            oyunbittiText.text = "OYUN BİTTİ";
+        }
+    }
+}
---
+++ sourcecode/RollingBall/Assets/codes/cameracontroll.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameracontroll : MonoBehaviour
+{
+    public GameObject ball;
+    Vector3 distance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        distance = transform.position - ball.transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = ball.transform.position + distance;
+    }
+}
